fc01fa8777067da2c33c8e047d386662745b3efa
Airat <airatm@live.ru>
2024-07-10 00:02:33 +0500

    Интерфейсы для реализации

---
+++ PlainCheckContracts/Interfaces/ILine.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlainCheckContracts.Interfaces
+{
+    /// <summary>
+    /// Интерфейс для реализации линий
+    /// </summary>
+    public interface ILine
+    {
+        /// <summary>
+        ///  Установка цвета линии в палитре RGBA в диапазоне 0-255
+        /// </summary>
+        /// <param name="red">Значение красного</param>
+        /// <param name="green">Значение зеленого</param>
+        /// <param name="blue">Значение синего</param>
+        /// <param name="alpha">Значение прозрачности (альфа-канал)</param>
+        void SetColor(byte red, byte green, byte blue, byte alpha);
+    }
+}
---
+++ PlainCheckLib/Implementations/LineCheck.cs
@@ -0,0 +1,14 @@
+﻿using PlainCheckContracts.Interfaces;
+using System;
+using System.Threading.Tasks;
+
+namespace PlainCheckLib.Implementations
+{
+    public class LineCheck : ILineCheck
+    {
+        public async Task<bool> IsLineInRectangle()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
